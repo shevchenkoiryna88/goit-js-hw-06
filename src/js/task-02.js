a2f9b88ecf333e2096c3870478876bd411f596ce
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-18 23:23:25 +0200

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+
+const mainList = document.querySelector("#ingredients")
+
+
+ingredients.map(ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  mainList.append(item)
+  return item;
+}
+)
+
+
+
+
+
