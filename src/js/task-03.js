2e8d143a00f47b0702418c8bf3f8dfe3a67e24f4
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-20 18:56:23 +0200

    task 3 не могу понять как правильно вписать данніе

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,3 +1,4 @@
+// неаонятно как это написать правильно
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,26 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery")
+console.log(gallery);
+
+let list = ""
+images.forEach(element => {
+  list += "<li>
+  <img 
+  src = "${element.url}"
+  alt="${element.alt}">
+  </li>"
+
+} );
+
+
+
+gallery.insertAdjacentHTML("beforeend", list);
+
+gallery.classList.add("gallery-style");
+gallery.style.margin = "0"
+gallery.style.padding = "0"
+gallery.style.display = "flex"
+gallery.style.gap = "15px"
