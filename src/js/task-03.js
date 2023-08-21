e5730c542e4a08c755fffafb3a57affa78bbc367
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-21 09:50:09 +0200

    Task3(finisfed)

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,4 +1,4 @@
-// неаонятно как это написать правильно
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -17,22 +17,24 @@ const images = [
 const gallery = document.querySelector(".gallery")
 console.log(gallery);
 
-let list = ""
+let list = ``;
 images.forEach(element => {
-  list += "<li>
-  <img 
-  src = "${element.url}"
-  alt="${element.alt}">
-  </li>"
+  list += ` <li>       
+  <img src="${element.url}" 
+  alt="${element.alt}" 
+  width="300" 
+  height="200">
+       </li>`
+}
+);
 
-} );
 
-
-
-gallery.insertAdjacentHTML("beforeend", list);
+gallery.insertAdjacentHTML("afterbegin", list);
 
 gallery.classList.add("gallery-style");
+
 gallery.style.margin = "0"
 gallery.style.padding = "0"
+gallery.style.listStyle = "none";
 gallery.style.display = "flex"
 gallery.style.gap = "15px"
