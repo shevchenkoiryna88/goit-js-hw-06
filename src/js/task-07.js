5dab5b71f4da53ba45e8674a1201e29ce2d528fd
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-21 14:55:50 +0200

    Task7not finished

---
+++ src/js/task-07.js
@@ -0,0 +1,10 @@
+const controller = document.querySelector("#font-size-control")
+const text = document.querySelector("#text")
+
+controller.addEventListener("input", () => {
+    const textSize = controller.value;
+    console.log(textSize);
+
+    text.style.fontSize = `${fontSize}px`
+}
+);
