4e6d6483912fb6dd0da752016c32b00322ceda92
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-23 19:34:13 +0200

    udate task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,26 +1,30 @@
-// Отримуємо посилання на елемент форми 
-const loginForm = document.querySelector(".login-form");
+// Отримати посилання на форму
+let form = document.querySelector(".login-form");
 
-// Додаємо слухача події submit до елемента loginForm 
-loginForm.addEventListener("submit", (event) => {
-    // Запобігаємо перезавантаженню сторінки 
-    event.preventDefault();
-    // Отримуємо об’єкт з елементами форми 
-    const formElements = event.currentTarget.elements;
+// Додати обробник події submit
+form.addEventListener("submit", function(event) {
+  // Запобігти перезавантаженню сторінки
+  event.preventDefault();
 
-    // Отримуємо значення полів email і password
-    const email = formElements.email.value;
-    const password = formElements.password.value;
-    // Перевіряємо, чи є незаповнені поля 
-    if (!email || !password) {
-        // Якщо так, то виводимо alert з попередженням 
-        alert("Everything should be filled");
-        return;
-    }
-    // Створюємо об’єкт із введеними даними 
-    const formData = { email, password, };
-    // Виводимо об’єкт в консоль 
-    console.log(formData); 
-    // Очищаємо значення полів форми 
-    loginForm.reset();
+  // Отримати значення полів
+  let email = form.elements.email.value;
+  let password = form.elements.password.value;
+
+  // Перевірити, чи всі поля заповнені
+  if (!email || !password) {
+    // Вивести попередження
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    // Створити об'єкт із даними
+    let data = {
+      email: email,
+      password: password
+    };
+
+    // Вивести об'єкт в консоль
+    console.log(data);
+
+    // Очистити значення полів
+    form.reset();
+  }
 });
