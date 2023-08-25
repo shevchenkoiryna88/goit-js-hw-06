f1ab759820b8ac8da246b2a5fd465a2aae3ed993
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-25 22:03:11 +0200

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+
+// const button = document.querySelector(".change-color");
+
+// const bodyColor = document.body.style.backgroundColor;
+
+// const color = document.querySelector(".color").innerHTML;
+
