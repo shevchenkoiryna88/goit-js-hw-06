4e18011dbc3659f670e02e8b4b140f2501db6bee
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-08-25 21:00:20 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,30 +1,23 @@
-// Отримати посилання на форму
 let form = document.querySelector(".login-form");
 
-// Додати обробник події submit
-form.addEventListener("submit", function(event) {
-  // Запобігти перезавантаженню сторінки
-  event.preventDefault();
 
-  // Отримати значення полів
+form.addEventListener("submit", function(event) {
+
+  event.preventDefault();// без перезагрузки
+
   let email = form.elements.email.value;
   let password = form.elements.password.value;
 
-  // Перевірити, чи всі поля заповнені
   if (!email || !password) {
-    // Вивести попередження
-    alert("Всі поля повинні бути заповнені!");
+    alert("Everything should be filled!")
   } else {
-    // Створити об'єкт із даними
-    let data = {
+
+    let result = {
       email: email,
-      password: password
+      password: password,
     };
 
-    // Вивести об'єкт в консоль
-    console.log(data);
-
-    // Очистити значення полів
-    form.reset();
+    console.log(result);
+    form.reset(); //очистим поля
   }
 });
