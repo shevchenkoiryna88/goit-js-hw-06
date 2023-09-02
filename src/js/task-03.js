be7b45e73308ed5cd4ee0bd024ece86db140a0ce
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-09-02 15:56:27 +0200

    Task3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,4 +1,5 @@
 
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -15,26 +16,18 @@ const images = [
 ];
 
 const gallery = document.querySelector(".gallery")
-console.log(gallery);
 
-let list = ``;
-images.forEach(element => {
-  list += ` <li>       
-  <img src="${element.url}" 
-  alt="${element.alt}" 
-  width="300" 
-  height="200">
-       </li>`
+images.map(element => {
+  const liWithImg = `<li><img src="${element.url}" alt="${element.url}" width = 300px></li>`;
+
+  gallery.insertAdjacentHTML("beforeend", liWithImg);
 }
-);
-
-
-gallery.insertAdjacentHTML("afterbegin", list);
-
-gallery.classList.add("gallery-style");
+)
 
 gallery.style.margin = "0"
 gallery.style.padding = "0"
 gallery.style.listStyle = "none";
 gallery.style.display = "flex"
 gallery.style.gap = "15px"
+
+console.log(gallery);
