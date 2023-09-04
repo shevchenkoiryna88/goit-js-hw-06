5fe4954c5b1e7034736372194f340795d4c1e10f
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-09-04 16:25:03 +0200

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,23 +1,24 @@
-let form = document.querySelector(".login-form");
 
 
-form.addEventListener("submit", function(event) {
+let form = document.querySelector(".login-form")
 
-  event.preventDefault();// без перезагрузки
+form.addEventListener("submit", test)
 
+function test(event) {
+  event.preventDefault();
+  
   let email = form.elements.email.value;
   let password = form.elements.password.value;
-
-  if (!email || !password) {
-    alert("Everything should be filled!")
-  } else {
-
-    let result = {
-      email: email,
-      password: password,
-    };
-
-    console.log(result);
-    form.reset(); //очистим поля
-  }
-});
+if ( !email || !password ) {
+  alert("Everything should be filled!");
+} else {
+  let result = {
+    email: email,
+    password: password,
+    
+  };  
+  // console.log(result);
+  form.reset()
+}
+  
+}
