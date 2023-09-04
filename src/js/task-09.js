036b2ed414f7c0e6989c590d123a9702dc07f885
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-09-04 17:06:26 +0200

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,13 +1,28 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+const body = document.querySelector("body")
+
+const btn = document.querySelector(".change-color");
+
+const span = document.querySelector(".color")
 
 
-// const button = document.querySelector(".change-color");
+btn.addEventListener("click", changeColor);
 
-// const bodyColor = document.body.style.backgroundColor;
+function changeColor () {
+    let newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor;
+    span.textContent = newColor;
 
-// const color = document.querySelector(".color").innerHTML;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+// console.log(body);
+// console.log(span);
+// console.log(btn);
 
