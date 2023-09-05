d1f3655a33e33ede279626e09f2d5bddef9cd5d0
Iryna Shevchenko <shev.iryna88@gmail.com>
2023-09-05 13:05:36 +0200

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,14 @@
 
 
 const ulCategories = document.querySelector("#categories")
-console.log(`Number of categories: ${ulCategories.children.length}`);//Number of categories
+console.log(`Number of categories: ${Number(ulCategories.children.length)}`);//Number of categories
  
 const category = document.querySelector("h2").textContent //Animals
-console.log(category);
 
 const elements = document.querySelectorAll(".item");
-console.log(elements);
-
 
 elements.forEach(element => {  
-  console.log(`Category : ${category}`);
-  console.log(`Elements: ${elements.length}`);
+    console.log(`Category : ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
 } 
-  )
+)
